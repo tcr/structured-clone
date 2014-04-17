9ce93fceaa060b31e094dd3cc3643578af658f57
Tim Cameron Ryan <id@timryan.org>
2014-04-17 12:24:14 -0400

    Converts to two-spaces.

--- serialize.js
+++ serialize.js
@@ -3,18 +3,18 @@
 
 function encodeRegExp (regexp)
 {
-    var flags = '';
-    if (regexp.global) flags += 'g';
-    if (regexp.multiline) flags += 'm';
-    if (regexp.ignoreCase) flags += 'i';
-    return [flags, regexp.source].join(',');
+  var flags = '';
+  if (regexp.global) flags += 'g';
+  if (regexp.multiline) flags += 'm';
+  if (regexp.ignoreCase) flags += 'i';
+  return [flags, regexp.source].join(',');
 }
 
 function decodeRegExp (str)
 {
-    var flags = str.match(/^[^,]*/)[0];
-    var source = str.substr(flags.length + 1);
-    return new RegExp(source, flags);
+  var flags = str.match(/^[^,]*/)[0];
+  var source = str.substr(flags.length + 1);
+  return new RegExp(source, flags);
 }
 
 
@@ -22,73 +22,73 @@ function decodeRegExp (str)
 
 function derez(value, path, objects, paths, buffers)
 {
-    if (Buffer.isBuffer(value)) {
-        var start = Buffer.concat(buffers).length;
-        buffers.push(value);
-        return '\x10b' + [start, value.length].join(',')
-    }
-    if (value instanceof Date) {
-        return '\x10d' + value.toJSON();
-    }
-    if (value instanceof RegExp) {
-        return '\x10r' + encodeRegExp(value);
-    }
-    if (typeof value == 'string') {
-        return value.charAt(0) == '\x10' ? '\x10s' + value : value;
-    }
+  if (Buffer.isBuffer(value)) {
+    var start = Buffer.concat(buffers).length;
+    buffers.push(value);
+    return '\x10b' + [start, value.length].join(',')
+  }
+  if (value instanceof Date) {
+    return '\x10d' + value.toJSON();
+  }
+  if (value instanceof RegExp) {
+    return '\x10r' + encodeRegExp(value);
+  }
+  if (typeof value == 'string') {
+    return value.charAt(0) == '\x10' ? '\x10s' + value : value;
+  }
 
-    var i,          // The loop counter
-        name,       // Property name
-        nu;         // The new object or array
+  var i,          // The loop counter
+    name,       // Property name
+    nu;         // The new object or array
 
 // typeof null === 'object', so go on if this value is really an object but not
 // one of the weird builtin objects.
 
-    if (typeof value === 'object' && value !== null &&
-        !(value instanceof Boolean) &&
-        !(value instanceof Number)  &&
-        !(value instanceof String)) {
+  if (typeof value === 'object' && value !== null &&
+    !(value instanceof Boolean) &&
+    !(value instanceof Number)  &&
+    !(value instanceof String)) {
 
 // If the value is an object or array, look to see if we have already
 // encountered it. If so, return a $ref/path object. This is a hard way,
 // linear search that will get slower as the number of unique objects grows.
 
-        i = objects.indexOf(value);
-        if (i !== -1) {
-            return '\x10j' + paths[i];
-        }
+    i = objects.indexOf(value);
+    if (i !== -1) {
+      return '\x10j' + paths[i];
+    }
 
 // Otherwise, accumulate the unique value and its path.
 
-        objects.push(value);
-        paths.push(path);
+    objects.push(value);
+    paths.push(path);
 
 // If it is an array, replicate the array.
 
-        if (Array.isArray(value)) {
-            nu = [];
-            for (i = 0; i < value.length; i += 1) {
-                nu[i] = derez(value[i],
-                    path + '[' + i + ']',
-                	objects, paths, buffers);
-            }
-        } else {
+    if (Array.isArray(value)) {
+      nu = [];
+      for (i = 0; i < value.length; i += 1) {
+        nu[i] = derez(value[i],
+          path + '[' + i + ']',
+          objects, paths, buffers);
+      }
+    } else {
 
 // If it is an object, replicate the object.
 
-            nu = {};
-            for (name in value) {
-                if (Object.prototype.hasOwnProperty.call(value, name) && value != '__proto__') {
-                    nu[name] = derez(value[name],
-                        path + '[' + JSON.stringify(name) + ']',
-                        objects, paths, buffers);
-                }
-            }
+      nu = {};
+      for (name in value) {
+        if (Object.prototype.hasOwnProperty.call(value, name) && value != '__proto__') {
+          nu[name] = derez(value[name],
+            path + '[' + JSON.stringify(name) + ']',
+            objects, paths, buffers);
         }
-        return nu;
+      }
     }
+    return nu;
+  }
 
-    return value;
+  return value;
 }
 
 
@@ -114,102 +114,102 @@ function rerez($, $$)
 //      return JSON.retrocycle(JSON.parse(s));
 // produces an array containing a single element which is the array itself.
 
-    var px =
-        /^\${1,4}(?:\[(?:\d+|\"(?:[^\\\"\u0000-\u001f]|\\([\\\"\/bfnrt]|u[0-9a-zA-Z]{4}))*\")\])*$/;
+  var px =
+    /^\${1,4}(?:\[(?:\d+|\"(?:[^\\\"\u0000-\u001f]|\\([\\\"\/bfnrt]|u[0-9a-zA-Z]{4}))*\")\])*$/;
 
-    function redo (item) {
-        if (typeof item == 'string' && item.charAt(0) == '\x10') {
-            switch (item.charAt(1)) {
-            case 's': return item.substr(2);
-            case 'b':
-                var bounds = item.substr(2).split(',', 2);
-                return $$.slice(bounds[0] || 0, (bounds[0] || 0) + (bounds[1] || [0]));
-            case 'd': return new Date(item.substr(2));
-            case 'r': return decodeRegExp(item.substr(2));
-            case 'j':
-                var path = item.substr(2);
-                if (px.test(path)) {
-                    return eval(path);
-                }
-            default: return null;
-            }
+  function redo (item) {
+    if (typeof item == 'string' && item.charAt(0) == '\x10') {
+      switch (item.charAt(1)) {
+      case 's': return item.substr(2);
+      case 'b':
+        var bounds = item.substr(2).split(',', 2);
+        return $$.slice(bounds[0] || 0, (bounds[0] || 0) + (bounds[1] || [0]));
+      case 'd': return new Date(item.substr(2));
+      case 'r': return decodeRegExp(item.substr(2));
+      case 'j':
+        var path = item.substr(2);
+        if (px.test(path)) {
+          return eval(path);
         }
-
-        if (item && typeof item === 'object') {
-            rez(item, $$);
-        }
-        return item;
+      default: return null;
+      }
     }
 
-    function rez(value) {
+    if (item && typeof item === 'object') {
+      rez(item, $$);
+    }
+    return item;
+  }
+
+  function rez(value) {
 
 // The rez function walks recursively through the object looking for $ref
 // properties. When it finds one that has a value that is a path, then it
 // replaces the $ref object with a reference to the value that is found by
 // the path.
 
-        var i, name;
+    var i, name;
 
-        if (value && typeof value === 'object') {
-            if (Array.isArray(value)) {
-                for (i = 0; i < value.length; i += 1) {
-                    value[i] = redo(value[i]);
-                }
-            } else {
-                for (name in value) {
-                	value[name] = redo(value[name]);
-                }
-            }
+    if (value && typeof value === 'object') {
+      if (Array.isArray(value)) {
+        for (i = 0; i < value.length; i += 1) {
+          value[i] = redo(value[i]);
         }
+      } else {
+        for (name in value) {
+          value[name] = redo(value[name]);
+        }
+      }
     }
+  }
 
-    $ = redo($)
+  $ = redo($)
 
-    return $;
+  return $;
 };
 
 
 // Public API
 
 exports.serialize = function (object) {
-    var objects = [],   // Keep a reference to each unique object or array
-        paths = [],     // Keep the path to each unique object or array
-        buffers = [];   // Returned buffers
+  var objects = [],   // Keep a reference to each unique object or array
+    paths = [],     // Keep the path to each unique object or array
+    buffers = [];   // Returned buffers
 
-    var nilbyte = new Buffer([0x00]);
+  var nilbyte = new Buffer([0x00]);
 
-    if (Buffer.isBuffer(object)) {
-        return Buffer.concat([
-            nilbyte,
-            object
-        ]);
-    }
-
-    var json = new Buffer(JSON.stringify(derez(object, '$', objects, paths, buffers)));
-    if (buffers.length == 0) {
-        return json;
-    }
-
+  if (Buffer.isBuffer(object)) {
     return Buffer.concat([
-        json,
-        nilbyte,
-        Buffer.concat(buffers)
+      nilbyte,
+      object
     ]);
+  }
+
+  var json = new Buffer(JSON.stringify(derez(object, '$', objects, paths, buffers)));
+  if (buffers.length == 0) {
+    return json;
+  }
+
+  return Buffer.concat([
+    json,
+    nilbyte,
+    Buffer.concat(buffers)
+  ]);
 }
 
 exports.deserialize = function (buf) {
-    for (var i = 0; i <= buf.length; i++) {
-        if (buf[i] == 0x00 || buf[i] == null) {
-            break;
-        }
+  for (var i = 0; i <= buf.length; i++) {
+    if (buf[i] == 0x00 || buf[i] == null) {
+      break;
     }
-    var jsonbuf = buf.slice(0, i);
-    var bufbuf = buf.slice(i + 1);
+  }
+  var jsonbuf = buf.slice(0, i);
+  var bufbuf = buf.slice(i + 1);
 
-    // Shortcut for only encoding a root buffer. 
-    if (jsonbuf.length == 0) {
-        return bufbuf;
-    }
+  // Shortcut for only encoding a root buffer. 
+  if (jsonbuf.length == 0) {
+    return bufbuf;
+  }
 
-    return rerez(JSON.parse(jsonbuf.toString('utf-8')), bufbuf);
+  return rerez(JSON.parse(jsonbuf.toString('utf-8')), bufbuf);
 }
--- test/test.js
+++ test/test.js
@@ -1,85 +1,85 @@
 var test = require('tape');
 
 test('deep cloning test', function (t) {
-	t.plan(12)
+  t.plan(12)
 
-	var clone = require('../')
+  var clone = require('../')
 
-	var a = { }
-	a.b = a
-	a.c = { $ref: '$' }
-	a.d = new Buffer([0xde, 0xad])
-	a.e = [ a, a.b ]
-	a.f = new Date()
-	a.g = /ab+a/i
+  var a = { }
+  a.b = a
+  a.c = { $ref: '$' }
+  a.d = new Buffer([0xde, 0xad])
+  a.e = [ a, a.b ]
+  a.f = new Date()
+  a.g = /ab+a/i
 
-	var a = clone(a);
+  var a = clone(a);
 
-	t.ok(a.b == a);
-	t.ok(a.c.$ref == '$')
-	t.ok(Buffer.isBuffer(a.d))
-	t.ok(a.d[0] == 0xde)
-	t.ok(a.d[1] == 0xad)
-	t.ok(a.d.length == 2);
-	t.ok(Array.isArray(a.e));
-	t.ok(a.e.length == 2);
-	t.ok(a.e[0] == a);
-	t.ok(a.e[1] == a.b);
-	t.ok(a.f instanceof Date);
-	t.ok(a.g instanceof RegExp);
+  t.ok(a.b == a);
+  t.ok(a.c.$ref == '$')
+  t.ok(Buffer.isBuffer(a.d))
+  t.ok(a.d[0] == 0xde)
+  t.ok(a.d[1] == 0xad)
+  t.ok(a.d.length == 2);
+  t.ok(Array.isArray(a.e));
+  t.ok(a.e.length == 2);
+  t.ok(a.e[0] == a);
+  t.ok(a.e[1] == a.b);
+  t.ok(a.f instanceof Date);
+  t.ok(a.g instanceof RegExp);
 })
 
 test('serializing test', function (t) {
-	t.plan(14)
+  t.plan(14)
 
-	var clone = require('../')
+  var clone = require('../')
 
-	var a = { }
-	a.b = a
-	a.c = { $ref: '$' }
-	a.d = new Buffer([0xde, 0xad])
-	a.e = [ a, a.b ]
-	a.f = new Date()
-	a.g = /ab+a/i
+  var a = { }
+  a.b = a
+  a.c = { $ref: '$' }
+  a.d = new Buffer([0xde, 0xad])
+  a.e = [ a, a.b ]
+  a.f = new Date()
+  a.g = /ab+a/i
 
-	var buf = clone.serialize(a);
-	t.ok(buf.length, 'Buffer has length')
-	t.ok(Buffer.isBuffer(buf), 'Buffer has length')
+  var buf = clone.serialize(a);
+  t.ok(buf.length, 'Buffer has length')
+  t.ok(Buffer.isBuffer(buf), 'Buffer has length')
 
-	var a = clone.deserialize(buf);
+  var a = clone.deserialize(buf);
 
-	t.ok(a.b == a);
-	t.ok(a.c.$ref == '$')
-	t.ok(Buffer.isBuffer(a.d))
-	t.ok(a.d[0] == 0xde)
-	t.ok(a.d[1] == 0xad)
-	t.ok(a.d.length == 2);
-	t.ok(Array.isArray(a.e));
-	t.ok(a.e.length == 2);
-	t.ok(a.e[0] == a);
-	t.ok(a.e[1] == a.b);
-	t.ok(a.f instanceof Date);
-	t.ok(a.g instanceof RegExp);
+  t.ok(a.b == a);
+  t.ok(a.c.$ref == '$')
+  t.ok(Buffer.isBuffer(a.d))
+  t.ok(a.d[0] == 0xde)
+  t.ok(a.d[1] == 0xad)
+  t.ok(a.d.length == 2);
+  t.ok(Array.isArray(a.e));
+  t.ok(a.e.length == 2);
+  t.ok(a.e[0] == a);
+  t.ok(a.e[1] == a.b);
+  t.ok(a.f instanceof Date);
+  t.ok(a.g instanceof RegExp);
 })
 
 test('deep copy root object', function (t) {
-	t.plan(3);
+  t.plan(3);
 
-	var clone = require('../');
+  var clone = require('../');
 
-	var buf = clone(new Buffer([0xca, 0xfe, 0xba, 0xbe]));
-	t.ok(Buffer.isBuffer(buf));
-	t.ok(buf.length == 4);
-	t.ok(buf.readUInt32BE(0) == 0xcafebabe);
+  var buf = clone(new Buffer([0xca, 0xfe, 0xba, 0xbe]));
+  t.ok(Buffer.isBuffer(buf));
+  t.ok(buf.length == 4);
+  t.ok(buf.readUInt32BE(0) == 0xcafebabe);
 });
 
 test('serializing root object', function (t) {
-	t.plan(3);
+  t.plan(3);
 
-	var clone = require('../');
+  var clone = require('../');
 
-	var buf = clone.deserialize(clone.serialize(new Buffer([0xca, 0xfe, 0xba, 0xbe])));
-	t.ok(Buffer.isBuffer(buf));
-	t.ok(buf.length == 4);
-	t.ok(buf.readUInt32BE(0) == 0xcafebabe);
+  var buf = clone.deserialize(clone.serialize(new Buffer([0xca, 0xfe, 0xba, 0xbe])));
+  t.ok(Buffer.isBuffer(buf));
+  t.ok(buf.length == 4);
+  t.ok(buf.readUInt32BE(0) == 0xcafebabe);
 });
